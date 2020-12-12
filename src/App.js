894b3542c7a3923544ba9d9498171ea5455991a6
Manazon-010101 <74126115 +Manazon-010101@users.noreply.github.com>
2020-12-12 14:42:09 +0900

    create class componet and functional component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <label htmlFor="bar">bar</label>
-        <input type="text" onChange={() => {console.log("I am clicked.")}} />
-      </React.Fragment>
-    )
-  }
+// このApp.jsにはAppと言うclassがComponentを継承して存在している
+// ↓このAppをクラスコンポーネントと呼ぶ
+// class App extends Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         <input type="text" onChange={() => {console.log("I am clicked.")}} />
+//       </React.Fragment>
+//     )
+//   }
+// }
+
+// 関数コンポーネントは関数をただ定義しているだけ
+// returnとしてJSXを返している
+const App = () => {
+  return (
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  )
+}
+
+const Cat = () => {
+  return <div>Meau!</div>
 }
 
 export default App;
