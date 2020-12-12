a3e23e381150f44cd331b2f289d8dc56cb9cbcd4
Manazon-010101 <74126115 +Manazon-010101@users.noreply.github.com>
2020-12-12 19:51:53 +0900

    implement component with connect function

--- src/index.js
+++ src/index.js
@@ -9,16 +9,8 @@ import reducer from './reducers'
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
-// ここで作られるstoreはアプリケーション内部で唯一になる
-// そしてアプリケーション内の全てのstateはこのstoreに集約されている。
+
 const store = createStore(reducer)
-
-// ここで作成したstoreがアプリケーション内のどのコンポーネントからも参照できるようにする
-// そのために利用するのが、Provider
-// 実装は簡単で、既存のコンポーネントをProviderコンポーネントでラップして、
-// store属性にさっき作成したstoreを渡してあげるだけ
-// こうすることでアプリケーション内の全ての階層のコンポーネントでstoreが使えるようになる。
-
 
 ReactDOM.render(
     <Provider store={store}>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,3 @@
-// このindex.jsではアプリケーション内での全てのreducerを総括する
-// つまり全てのreducerを1つに結合する。
-
-// 今回の場合はreducerが1つしかないが、
-// 普段のアプリケーションでは複数のreducerを書く事になる
-// その時はcombineReducersの引数のオブジェクトに
-// ↓そのアプリケーションが使うreducerを列挙するだけで済む。
-// export default combineReducers({ foo, bar, baz })
-
-        // ↓reducerを結合する為の関数
 import {combineReducers} from 'redux'
 import count from './count'
 
