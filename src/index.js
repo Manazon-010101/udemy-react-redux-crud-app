ab4153256d98ee510eac7794975b37ff867ade94
Manazon-010101 <74126115 +Manazon-010101@users.noreply.github.com>
2020-12-12 19:09:01 +0900

    implement store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 import './index.css';
-import App from './App';
+import reducer from './reducers'
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
+// ここで作られるstoreはアプリケーション内部で唯一になる
+// そしてアプリケーション内の全てのstateはこのstoreに集約されている。
+const store = createStore(reducer)
+
+// ここで作成したstoreがアプリケーション内のどのコンポーネントからも参照できるようにする
+// そのために利用するのが、Provider
+// 実装は簡単で、既存のコンポーネントをProviderコンポーネントでラップして、
+// store属性にさっき作成したstoreを渡してあげるだけ
+// こうすることでアプリケーション内の全ての階層のコンポーネントでstoreが使えるようになる。
+
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Provider store={store}>
+      <App />
+    </Provider>,
   document.getElementById('root')
 );
 
