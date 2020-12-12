940046a645155583368c09486e78b8059a999f8f
Manazon-010101 <74126115 +Manazon-010101@users.noreply.github.com>
2020-12-12 15:19:26 +0900

    learn props of component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,35 @@
-// import React, { Component } from 'react';
 import React from 'react';
 
-// このApp.jsにはAppと言うclassがComponentを継承して存在している
-// ↓このAppをクラスコンポーネントと呼ぶ
-// class App extends Component {
-//   render() {
-//     return (
-//       <React.Fragment>
-//         <label htmlFor="bar">bar</label>
-//         <input type="text" onChange={() => {console.log("I am clicked.")}} />
-//       </React.Fragment>
-//     )
-//   }
-// }
+// 親のコンポーネントから子のコンポーネントに対してあるデータを渡したい時に
+// propsを使ってデータを渡してあげる。
 
-// 関数コンポーネントは関数をただ定義しているだけ
-// returnとしてJSXを返している
 const App = () => {
+  // 簡略化する為、
+  // User情報を一括で変数で管理してそれをmap処理を使って順番に回しながら各Userのプロファイルを表示する
+  const profiles = [
+    {name: "Taro", age: 10},
+    {name: "Hanako", age: 5},
+  ]
   return (
     <div>
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
+      {/* JSのコードなので{}で囲む */}
+      {
+                            // ↓keyにindexを与えてあげる
+        profiles.map((profile, index) => {
+          // ↓Reactにはバーチャルdomが存在していて、
+          // その中でどのdomが変更になったのかを管理している
+          // その変更点のみを実際のdomに反映していくと言う機構がある
+          // それぞれの値にkeyを与えてあげて、必要最低限なdomの範囲をReactが管理している
+          return <User name={profile.name} age={profile.age} key={index}/>
+        })
+      }
     </div>
   )
 }
 
-const Cat = () => {
-  return <div>Meau!</div>
+//            ↓nameとageを受け取ることができる
+const User = (props) => {
+return <div>Hi, i am {props.name}, and {props.age} years old! </div>
 }
 
 export default App;
