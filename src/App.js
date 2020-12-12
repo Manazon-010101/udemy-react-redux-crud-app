310e8f97c2512894267b456b4f3f4ea14b659187
Manazon-010101 <74126115 +Manazon-010101@users.noreply.github.com>
2020-12-12 16:29:31 +0900

    learn prop-types

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React from 'react';
-
-// 親のコンポーネントから子のコンポーネントに対してあるデータを渡したい時に
-// propsを使ってデータを渡してあげる。
+import PropTypes from 'prop-types';
 
 const App = () => {
-  // 簡略化する為、
-  // User情報を一括で変数で管理してそれをmap処理を使って順番に回しながら各Userのプロファイルを表示する
   const profiles = [
     {name: "Taro", age: 10},
     {name: "Hanako", age: 5},
+    {name: "NoName", age: 3}
   ]
   return (
     <div>
-      {/* JSのコードなので{}で囲む */}
       {
-                            // ↓keyにindexを与えてあげる
         profiles.map((profile, index) => {
-          // ↓Reactにはバーチャルdomが存在していて、
-          // その中でどのdomが変更になったのかを管理している
-          // その変更点のみを実際のdomに反映していくと言う機構がある
-          // それぞれの値にkeyを与えてあげて、必要最低限なdomの範囲をReactが管理している
           return <User name={profile.name} age={profile.age} key={index}/>
         })
       }
@@ -27,9 +18,15 @@ const App = () => {
   )
 }
 
-//            ↓nameとageを受け取ることができる
 const User = (props) => {
 return <div>Hi, i am {props.name}, and {props.age} years old! </div>
+}
+
+// 型チェック 受け取るプロパティの型がブレないように初めに定義しておく
+User.propTypes = {
+  name: PropTypes.string,
+                        // ↓ age属性が存在しないとダメですよと言うエラーを出すためにある物
+  age: PropTypes.number.isRequired
 }
 
 export default App;
